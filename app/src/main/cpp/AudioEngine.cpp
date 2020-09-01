6b56bf9b03005872c07142739b0a6f1fb062c6f6
Clay Bailey <claybailey98@gmail.com>
2020-09-01 11:34:13 -0400

    override onAudioReady

--- app/src/main/cpp/AudioEngine.cpp
+++ app/src/main/cpp/AudioEngine.cpp
@@ -4,7 +4,37 @@
 #include "AudioEngine.h"
 
 void AudioEngine::start() {
+    AudioStreamBuilder builder;
 
+    builder.setFormat(AudioFormat::Float);
+    builder.setChannelCount(1);
+    builder.setPerformanceMode(PerformanceMode::LowLatency);
+    builder.setSharingMode(SharingMode::Exclusive);//Do not mix audio with another app's (bypass mixer = lower latency)
+    builder.setCallback(this);
 
+    builder.openStream(&mStream);
 
+    //interrogate audio device for minimum amount of data it will read in one operation (burst)
+    //use two bursts to prevent underruns
+    mStream->setBufferSizeInFrames(mStream->getFramesPerBurst() * 2);
+
+    mStream->requestStart();
+}
+
+/*
+ * Overriding function inherited from AudioStreamCallback.
+ * Method called when the audio device needs more data.
+ *
+ * @AudioStream *oboeStream:
+ * The stream who wants more data.
+ *
+ * @void *audioData:
+ * Container array, either 16bit ints or float samples, to which the audio data will be written.
+ *
+ * @int32_t numFrames:
+ * Number of audio frames which need to be sent in the audioData container array.
+ */
+DataCallbackResult
+AudioEngine::onAudioReady(AudioStream *oboeStream, void *audioData, int32_t numFrames) {
+    return Stop;
 }
--- app/src/main/cpp/AudioEngine.h
+++ app/src/main/cpp/AudioEngine.h
@@ -10,10 +10,15 @@
 #include <oboe/Oboe.h>
 
 using namespace oboe;
-
-class AudioEngine {
+//AudioEngine inherits from AudioStreamCallback
+class AudioEngine : public AudioStreamCallback {
 public:
     void start();
+
+    DataCallbackResult
+    onAudioReady(AudioStream *oboeStream, void *audioData, int32_t numFrames) override;
+
+    AudioStream *mStream;
 };
 
 #endif //SENIOR_PROJECT_AUDIOENGINE_H
